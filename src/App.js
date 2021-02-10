e6989d6af4c5481361bfdcc3fcd185705867c12f
Julien <74821424+JulienT91@users.noreply.github.com>
2021-02-10 09:36:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
@@ -10,6 +11,7 @@ const App = () => {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/a-propos" component={About} />
+        <Route path="/contact" component={Contact} />
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
