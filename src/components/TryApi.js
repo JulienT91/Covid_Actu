a2d1b4b16b5ed97c29920a8335a6710a141a8ac3
Julien <74821424+JulienT91@users.noreply.github.com>
2021-02-11 10:01:32 +0100

    Update TryApi.js

--- src/components/TryApi.js
+++ src/components/TryApi.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const TryApi = () => {
-  const [data, setData] = useState(["hello"]);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=big&opening-date=2020-01-01;2021-01-01&api-key=TZAHIfno7jTnAUT23ErUAKczxIGihXuE"
+      )
+      .then((res) => setData(res.data));
 
-  return <div>{data}</div>;
+    console.log(data);
+  }, []);
+  return (
+    <div className="reviews">
+      <ul className="reviews-list">
+        {data.map((review) => (
+          <li>{review.display_title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default TryApi;
